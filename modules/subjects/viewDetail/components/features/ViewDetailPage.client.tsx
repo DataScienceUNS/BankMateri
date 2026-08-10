01238bfb38e84d62cad01989a8f3272c2c1bee7e
rafiarrafif <rafiarrafif05@gmail.com>
2026-08-10 21:00:00 +0700

    feat: material detail page

--- modules/subjects/viewDetail/components/features/ViewDetailPage.client.tsx
+++ modules/subjects/viewDetail/components/features/ViewDetailPage.client.tsx
@@ -1,5 +1,10 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/modules/shadcn/ui/badge";
+import { ArrowLeft, BookOpen } from "lucide-react";
+import Link from "next/link";
 import React from "react";
+import SearchAndFilter from "../ui/SearchAndFilter";
 
 type ViewDetailPageClientProps = {
   detail: {
@@ -24,7 +29,35 @@ type ViewDetailPageClientProps = {
 };
 
 const ViewDetailPageClient = ({ detail }: ViewDetailPageClientProps) => {
-  return <div>ViewDetailPage: {JSON.stringify(detail)}</div>;
+  return (
+    <div className="mt-8">
+      <Link
+        href="/subjects"
+        className="text-neutral-600 hover:text-neutral-900 text-sm cursor-pointer p-0 flex items-center gap-2"
+      >
+        <ArrowLeft size={16} /> All Subjects
+      </Link>
+
+      <header className="flex items-center gap-4 p-6 border border-neutral-200 rounded-xl mt-5">
+        <div style={{ background: `#${detail?.hex_color}` }} className="p-4 rounded-xl">
+          <BookOpen className="text-white" />
+        </div>
+        <div className="space-y-1.5">
+          <h1 className="font-semibold text-2xl">{detail?.name}</h1>
+          <div className="flex items-center gap-2">
+            <Badge variant="secondary">{detail?.code}</Badge>
+            <span className="text-neutral-500 text-sm">
+              Semester {detail?.term} · {detail?.materials.length} Materials
+            </span>
+          </div>
+        </div>
+      </header>
+
+      <main className="mt-8">
+        <SearchAndFilter />
+      </main>
+    </div>
+  );
 };
 
 export default ViewDetailPageClient;
