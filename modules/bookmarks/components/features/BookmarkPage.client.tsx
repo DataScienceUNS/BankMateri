5885db7ec47bdf5d0716afd59c7d11848c8939f4
rafiarrafif <rafiarrafif05@gmail.com>
2026-08-24 10:43:00 +0700

    feat: card for empty bookmark

--- modules/bookmarks/components/features/BookmarkPage.client.tsx
+++ modules/bookmarks/components/features/BookmarkPage.client.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { Material_Type } from "@/app/generated/prisma";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/modules/shadcn/ui/input-group";
+import { Search } from "lucide-react";
 import React from "react";
+import BookmarkEmptyCard from "../ui/BookmarkEmptyCard";
 
 type BookmarkPageClientProps = {
   bookmarks: {
@@ -27,7 +30,38 @@ type BookmarkPageClientProps = {
 };
 
 const BookmarkPageClient = ({ bookmarks }: BookmarkPageClientProps) => {
-  return <div>{JSON.stringify(bookmarks)}</div>;
+  return (
+    <div className="mt-10">
+      <header className="flex justify-between items-center">
+        <div>
+          <h1 className="text-[30px] text-neutral-800 font-semibold">Bookmarks</h1>
+          <h3 className="text-muted-foreground text-sm">You have {bookmarks.length} bookmarked resources.</h3>
+        </div>
+      </header>
+      <main className="mt-8">
+        <InputGroup className="max-w-md py-5 px-1">
+          <InputGroupInput className="ml-1" placeholder="Search..." />
+          <InputGroupAddon>
+            <Search />
+          </InputGroupAddon>
+        </InputGroup>
+        <div className="mt-5">
+          {bookmarks.length === 0 ? (
+            <BookmarkEmptyCard />
+          ) : (
+            <ul>
+              {bookmarks.map((bookmark) => (
+                <li key={bookmark.material.id}>
+                  <h4>{bookmark.material.title}</h4>
+                  <p>{bookmark.material.description}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default BookmarkPageClient;
